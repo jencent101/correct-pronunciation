8d823c59dc9d19bb84fe09d3a0359a3d0a83b4bc
I-am-Programmer-101 <92503713+I-am-Programmer-101@users.noreply.github.com>
2021-10-14 00:45:19 -0700

    Add files via upload

---
+++ Voice_Speak.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import messagebox
+import pyttsx3
+
+rt = Tk()
+rt.title("Voice Speak")
+
+engine = pyttsx3.init()
+def Enter():
+    entry_message = enter_entry.get()
+    engine.say(entry_message)
+    engine.runAndWait()
+
+def Reset():
+    enter_entry.delete(0, END)
+
+frame = LabelFrame(rt,padx=70, pady=70, bg="lightblue")
+enter_label  = Label(frame, text="Enter a string to say : ", bg="lightblue", fg="darkviolet")
+enter_entry  = Entry(frame, width=25)
+enter_button = Button(frame, text="Enter", padx=20, command=Enter, bg="lightblue", fg="blue")
+reset_button = Button(frame, text="Reset",padx=20, command=Reset, bg="lightblue", fg="green")
+exit_button  = Button(frame, text="Exit", padx=20, command=rt.destroy, bg="lightblue", fg="red")
+
+frame.pack(padx=25, pady=25)
+enter_label.grid(row=0, column=0)
+enter_entry.grid(row=0, column=1)
+enter_button.grid(row=1, column=0)
+reset_button.grid(row=1, column=1)
+exit_button.grid(row=1, column=2)
+
+rt.mainloop()
